e589d8b97468d864b717f00b5b2089192ee3bf77
Grooblesign <p.j.garner@gmail.com>
2014-04-01 21:59:35 +0100

    testing

--- src/main/java/uk/me/paulgarner/controller/HelloController.java
+++ src/main/java/uk/me/paulgarner/controller/HelloController.java
@@ -1,5 +1,7 @@
 package uk.me.paulgarner.controller;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +9,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 
+	@RequestMapping(value="/")
+	public String Index(Model model) {
+		model.addAttribute("datetime", new Date().toString());
+		
+		return "index";
+	}
+
 	@RequestMapping(value="/greeting")
 	public String SayHello(Model model) {
 		model.addAttribute("greeting", "Hello World");
+		model.addAttribute("datetime", new Date().toString());
 		
-		return "Hello";
+		return "hello";
 	}
 }
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<title>World Cop 2014</title>
+	</head>		
+	<body>
+		<h1>World Cup 2014</h1>
+		${datetime}
+	</body>
+</html>
