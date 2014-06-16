fabd1095c48e72e7cc6be5af9100121287e139e4
Grooblesign <p.j.garner@gmail.com>
2014-06-16 07:34:05 +0100

    'l'

--- src/main/java/uk/me/paulgarner/util/DataLoader.java
+++ src/main/java/uk/me/paulgarner/util/DataLoader.java
@@ -408,8 +408,8 @@ public class DataLoader {
 			match.setTime("19:00");
 			match.setVenue("Estádio do Maracanã, Rio de Janeiro");
 			
-			match.setTeam1Goals(-1);    
-			match.setTeam2Goals(-1);
+			match.setTeam1Goals(2);    
+			match.setTeam2Goals(1);
 
 			match.setTeam1GoalsAdam(2);
 			match.setTeam2GoalsAdam(1);
